a07639d7826c3a4f47b5182792a7a9936ec6434d
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2021-02-22 23:36:38 -0600

    First commit, 2 examples of Java streams

---
+++ src/main/java/JavaStreams2.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class JavaStreams2 {
+    public static void main(String[] args) throws IOException {
+
+        // 2. Integer Stream with skip
+        IntStream
+                .range(1, 10)
+                .skip(5)
+                .forEach(System.out::print);
+        System.out.println();
+    }
+}
