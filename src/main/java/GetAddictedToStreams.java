fb01fb2bfbd5a3acd07cf82d7f33fe3307dce320
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2021-02-25 21:42:54 -0600

    Add stream examples
     Examples are based on code in "Get a Taste of Lambdas and Get Addicted to Streams by Venkat Subramaniam" on youtube.

---
+++ src/main/java/GetAddictedToStreams.java
@@ -0,0 +1,28 @@
+import com.google.common.base.Stopwatch;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GetAddictedToStreams {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Stopwatch timer = Stopwatch.createStarted();
+        System.out.println(
+                // numbers.stream()
+                numbers.parallelStream()
+                       .filter(e -> e % 2 == 0)
+                       .mapToInt(GetAddictedToStreams::compute)
+                       .sum());
+        System.out.println("Method took: " + timer.stop());
+    }
+
+    public static int compute(int number) {
+        // assume this is a very time intensive function
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            //
+        }
+        return number * 2;
+    }
+}
---
+++ src/main/java/TasteOfLambdasGetAddictedToStreams.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TasteOfLambdasGetAddictedToStreams {
+    public static void main(String[] args) {
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5);
+//        printListOfIntA(numbers);
+//        System.out.println("");
+//        printListOfIntB(numbers);
+//        twoArgLambdaAMethodReference(numbers);
+//        twoArgLambdaBMethodReference(numbers);
+        //
+    }
+
+    private static void printListOfIntA(List<Integer> numbers) {
+        numbers.stream()
+               .map(String::valueOf)
+               .forEach(System.out::print);
+    }
+
+    private static void printListOfIntB(List<Integer> numbers) {
+        numbers.stream()
+               // obj -> String.valueOf(obj)
+               .map(String::valueOf)
+               // s -> s.toString()
+               .map(String::toString)
+               .forEach(System.out::print);
+    }
+
+    private static void twoArgLambdaAMethodReference(List<Integer> numbers) {
+        System.out.println(
+                numbers.stream()
+                       // .reduce(0, (total, e) -> Integer.sum(e, total)));
+                       .reduce(0, Integer::sum));
+    }
+
+    private static void twoArgLambdaBMethodReference(List<Integer> numbers) {
+        System.out.println(
+                numbers.stream()
+                       .map(String::valueOf)
+                       // .reduce("", (carry, str) -> carry.concat(str)));
+                       .reduce("", String::concat));
+    }
+
+    // given the values, double the even numbers and total
+    private static void doubleEvensAndSumLambda(List<Integer> numbers) {
+        System.out.println(
+                numbers.stream()
+                       .filter(e -> e % 2 == 0)
+                       .mapToInt(e -> e * 2)
+                       .sum());
+    }
+}
